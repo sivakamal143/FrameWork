6cad536c97cecf7eb6a4daf6eb8d63fac70e4dc0
admin <admin@DESKTOP-U5CRAMB>
2024-02-09 11:42:49 +0530

    ABCD

--- src/main/java/com/base/Testclass.java
+++ src/main/java/com/base/Testclass.java
@@ -3,8 +3,10 @@ package com.base;
 public class Testclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		int a;
+		System.out.println("sumathi");
 	}
 
 }
